19f7dbcc00832693cb48aca9bde5adfd4669095d
Haipeng Li <haipeng@stanford.edu>
2022-12-24 11:27:27 -0800

    update base, solver

--- toolbox-dev/base.py
+++ toolbox-dev/base.py
@@ -15,12 +15,12 @@ from multiprocessing import cpu_count
 import numpy as np
 
 
-class config(object):
+class Config(object):
     '''
         config class describes the configuration
     '''
 
-    def __init__(self, path, mpi_num, fig_aspect):
+    def __init__(self, path, mpi_num, fig_aspect = 1.0):
         '''
             initialize config class
 
@@ -35,8 +35,30 @@ class config(object):
         self.mpi_num = mpi_num
         self.fig_aspect = fig_aspect
 
+        # check the config parameters
+        self.__check__()
 
-class model(object):
+
+    def __check__(self):
+        '''
+            check the config parameters
+        '''
+        # check path format
+        if self.path[-1] != '/':
+            self.path += '/'
+
+        # check the path
+        if not os.path.exists(self.path):
+            print('Warning: working path {} does not exist, creating it now.'.format(self.path))
+            os.makedirs(self.path)
+
+        # check the number of mpi processes
+        if self.mpi_num > cpu_count()// 2:
+            print('Warning: number of mpi processes {} is larger than the number of CPUs, setting it maximum available CPUs to {}.'.format(self.mpi_num, cpu_count()//2))
+            self.mpi_num = cpu_count() // 2
+    
+
+class Model(object):
     '''
         model class describes the model for wavefield simulation
     '''
@@ -63,12 +85,12 @@ class model(object):
         self.dt = dt
         self.nt = nt
         self.pml = pml
-        self.rho = rho
-        self.vp = vp
+        self.rho = rho.copy()
+        self.vp = vp.copy()
 
-        # pml parameters
+        # pml parameters, free surface at the top
         self.nx_pml = self.nx + self.pml * 2
-        self.nz_pml = self.nz + self.pml  # free surface at the top
+        self.nz_pml = self.nz + self.pml
 
         # coordinates of grid points
         self.x = np.arange(0, self.nx * self.dx, self.dx)
@@ -92,70 +114,13 @@ class model(object):
 
         # check the dimensions of models
         if np.shape(self.vp) != (self.nx, self.nz):
-            raise ValueError('The dimensions of vp are not consistant with \
-                nx = {}, nz = {}'.format(self.nx, self.nz))
+            raise ValueError('The dimensions of vp are not consistant with nx = {}, nz = {}'.format(self.nx, self.nz))
        
         if np.shape(self.rho) != (self.nx, self.nz):
-            raise ValueError('The dimensions of rho are not consistant with \
-                nx = {}, nz = {}'.format(self.nx, self.nz))
-
-        # check path format
-        if self.path[-1] != '/':
-            self.path += '/'
-
-        # check the stability condition of 4-th order FD method: dt <= sqrt(3/8) * dx / vmax
-        dt_req = np.sqrt(3.0/8.0) * self.dx / np.max(self.vp)
-
-        if dt_req <= self.dt:
-            raise ValueError('the stability condition of 4-th order FD method \
-            is not satisfied: dt = %.4f ms > dt_required = %.4f ms'.format(dt*1000, dt_req*1000))
-
-        # check the numerical dispersion condition: dx <= vmin/(10*f0)
-        dx_req = np.min(self.vp) / self.f0 / 10.
-        f0_req = np.min(self.vp) / self.dx / 10.
-
-        if dx_req < self.dx:
-            print('Warning: modeling dispersion, dx = %.2f m > dx_required =  %.2f m' %(self.dx, dx_req))
-            print('Warning: modeling dispersion, f0 = %.2f Hz > f0_required = %.2f Hz' %(self.f0, f0_req))
-  
+            raise ValueError('The dimensions of rho are not consistant with nx = {}, nz = {}'.format(self.nx, self.nz))
 
 
-class receiver(object):
-    '''
-        receiver class describes the receiver geometry
-    '''
-
-    def __init__(self, coord, comp):
-        '''
-            initialize receiver class
-
-            input:
-                coord: coordinates of receivers, 2D array
-                comp: components of receivers, ['vx', 'vz', 'p']
-        '''
-
-        # basic parameters
-        self.coord = coord
-        self.comp = comp
-
-        # number of receivers
-        self.num = len(self.coord)
-
-        # check the receiver parameters
-        self.__check__()
-
-
-    def __check__(self):
-        '''
-            check the receiver parameters
-        '''
-
-        # check the dimensions of models
-        if self.rec_comp not in ['vx', 'vz', 'p']:
-            raise ValueError('rec_comp can only be vx, vz or p, or a combination of them')
-
-        
-class source(object):
+class Source(object):
     '''
         source class describes the source geometry
     '''
@@ -184,13 +149,58 @@ class source(object):
             check the source parameters
         '''
 
-        # check the dimensions of models
+        # check coordinates of sources
+        if self.coord.shape[-1] != 2:
+            raise ValueError('The coordinates of sources must be 2D array')
+
+        # check the wavelet
         if self.wavelet.shape[0] != self.num:
             raise ValueError('The number of sources is not consistant with \
                 the number of source wavelets')
 
 
-class preprocessor(object):
+class Receiver(object):
+    '''
+        receiver class describes the receiver geometry
+    '''
+
+    def __init__(self, coord, comp):
+        '''
+            initialize receiver class
+
+            input:
+                coord: coordinates of receivers, 2D array
+                comp: components of receivers, ['vx', 'vz', 'p']
+        '''
+
+        # basic parameters
+        self.coord = coord
+        self.comp = comp
+
+        # check the receiver parameters
+        self.__check__()
+
+
+    def __check__(self):
+        '''
+            check the receiver parameters
+        '''
+        # check coordinates of receivers
+        if self.coord.shape[-1] != 2:
+            raise ValueError('The coordinates of receivers must be 2D array')
+
+        # components of receivers must be a list
+        if not isinstance(self.comp, list):
+            raise ValueError('comp must be a list')
+
+        # check the components of receivers
+        for comp in self.comp:
+            if comp not in ['vx', 'vz', 'p']:
+                raise ValueError('comp can only be vx, vz or p, or a combination of them')
+
+
+
+class Processor(object):
     ''' 
         preprocessor class describes the data preprocessing
     '''
--- toolbox-dev/solver.py
+++ toolbox-dev/solver.py
@@ -13,10 +13,8 @@
 
 import os
 import subprocess
+import numpy as np
 from scipy import integrate
-
-from tools import cleandata
-from multiprocessing import cpu_count
 
 
 class Solver(object):
@@ -26,7 +24,7 @@ class Solver(object):
     def __init__(self, config, model, source, receiver):
         ''' Initialize solver
         '''
-        # basic class variables for solver        
+        # basic class variables for solver
         self.config = config
         self.model = model
         self.source = source
@@ -39,43 +37,7 @@ class Solver(object):
         self.__info__()
 
 
-
-    def __check__(self):
-        ''' Check the solver
-        '''
-    
-        # check the source location
-        if (self.source.coord[:,0].min() < self.model.x.min() or 
-            self.source.coord[:,0].max() > self.model.x.max() or
-            self.source.coord[:,1].min() < self.model.z.min() or 
-            self.source.coord[:,1].max() > self.model.z.max()):
-            raise ValueError('Source location out of model range.')
-        
-        # check the receiver location
-        if (self.receiver.xz[:,0].min() < self.model.x.min() or
-            self.receiver.xz[:,0].max() > self.model.x.max() or
-            self.receiver.xz[:,1].min() < self.model.z.min() or
-            self.receiver.xz[:,1].max() > self.model.z.max()):
-            raise ValueError('Receiver location out of model range.')
-
-        # check the mpiproc and set the number of CPUs
-        self.config.mpiproc = min([self.config.mpiproc, self.source.n, cpu_count() // 2])
-                
-
-    def __info__(self):
-        ''' Print solver information
-        '''
-        print('*****************************************************')
-        print('\n        Seismic Waveform Inversion Toolbox         \n')
-        print('*****************************************************\n')
-        print('Forward modeling : nx, nz = {}, {}'.format(self.model.nx, self.model.nz) )
-        print('Forward modeling : dx = {.1f} m'.format(self.model.dx))
-        print('Forward modeling : dt = {.2f} ms, {} steps'.format(self.model.dt * 1000, self.model.nt))
-        print('Forward modeling : %d shots run in mpi, %d CPU available'.format(self.system.mpiproc, cpu_count() // 2))
-
-
-
-    def forward(self, simu_type = 'obs', savesnap = 0):
+    def forward(self, simu_type = 'obs', save_snap = 0):
         ''' Forward solver
         '''
 
@@ -89,14 +51,14 @@ class Solver(object):
         # create working directory
         for isrc in range(self.source.srcn):
             ifolder = self.config.path + 'data/%s/src%d_snapshot'%(simu_type, isrc+1)
-            if not os.path.exists(ifolder) and savesnap == 1:
+            if not os.path.exists(ifolder) and save_snap == 1:
                 os.system('mkdir %s' % ifolder)
 
         # prepare the forward source
         src = integrate.cumtrapz(self.source.wavelet, axis=-1, initial=0)
 
         # write parameters and model files
-        self.write_parfile(simu_type, src, savesnap=savesnap)
+        self.write_parfile(simu_type, src, save_snap=save_snap)
 
         # check mpi and wavefield solver
         if os.system('which mpirun') != 0:
@@ -114,44 +76,97 @@ class Solver(object):
             raise ValueError('Forward solver crash')
 
 
-    def adjoint(self, savesnap = 0):
-        ''' Adjoint solver
-        '''
+    # def adjoint(self, savesnap = 0):
+    #     ''' Adjoint solver
+    #     '''
     
-        simu_type = 'adj'
+    #     simu_type = 'adj'
         
-        # change path and clean previous data (always, even empty)
-        cleandata(self.config.homepath + 'data/adj/')
+    #     # change path and clean previous data (always, even empty)
+    #     cleandata(self.config.homepath + 'data/adj/')
 
-        # create working directory
-        for isrc in range(self.source.srcn):
-            ifolder = self.config.homepath + 'data/%s/src%d_snapshot'%(simu_type, isrc+1)
-            if not os.path.exists(ifolder) and savesnap == 1:
-                os.system('mkdir %s' % ifolder)
+    #     # create working directory
+    #     for isrc in range(self.source.srcn):
+    #         ifolder = self.config.homepath + 'data/%s/src%d_snapshot'%(simu_type, isrc+1)
+    #         if not os.path.exists(ifolder) and savesnap == 1:
+    #             os.system('mkdir %s' % ifolder)
 
-        # prepare the forward source
-        src = integrate.cumtrapz(self.source.wavelet, axis=-1, initial=0)
+    #     # prepare the forward source
+    #     src = integrate.cumtrapz(self.source.wavelet, axis=-1, initial=0)
 
-        # write parameters and model files
-        self.write_parfile(simu_type, src, savesnap=savesnap)
+    #     # write parameters and model files
+    #     self.write_parfile(simu_type, src, savesnap=savesnap)
 
-        # check mpi and wavefield solver
-        if os.system('which mpirun') != 0:
-            raise ValueError('Cannot find mpirun, please check your mpi installation.')
-        if os.system('which fd2dmpi') != 0:
-            raise ValueError('Cannot find fd2dmpi, please check your wavefield solver installation.')
+    #     # check mpi and wavefield solver
+    #     if os.system('which mpirun') != 0:
+    #         raise ValueError('Cannot find mpirun, please check your mpi installation.')
+    #     if os.system('which fd2dmpi') != 0:
+    #         raise ValueError('Cannot find fd2dmpi, please check your wavefield solver installation.')
 
-        # submit job
-        os.chdir(self.config.homepath)
-        solver_cmd = 'mpirun -np %d  fd2dmpi par=%s' % (self.config.mpiproc, 
-                    self.config.homepath + 'parfile/forward_parfile/parfile')
-        status = subprocess.getstatusoutput(solver_cmd)
-        if status[0]:
-            print(status[1])
-            raise ValueError('Forward solver crash')
+    #     # submit job
+    #     os.chdir(self.config.homepath)
+    #     solver_cmd = 'mpirun -np %d  fd2dmpi par=%s' % (self.config.mpiproc, 
+    #                 self.config.homepath + 'parfile/forward_parfile/parfile')
+    #     status = subprocess.getstatusoutput(solver_cmd)
+    #     if status[0]:
+    #         print(status[1])
+    #         raise ValueError('Forward solver crash')
 
   
-    def dot_product(self, simu_type = 'obs'):
-        ''' Calculate dot product of two wavefields
+    # def dot_product(self, simu_type = 'obs'):
+    #     ''' Calculate dot product of two wavefields
+    #     '''
+    #     pass
+
+
+    def __check__(self):
+        ''' Check the solver
         '''
-        pass
+        
+        # check the stability condition (4-th order FD): dt <= sqrt(3/8) * dx / vmax
+        dt0 = np.sqrt(3.0/8.0) * self.model.dx / np.max(self.model.vp)
+        if dt0 <= self.model.dt:
+            raise ValueError('the stability condition of 4-th order FD method \
+            is not satisfied: dt = %.4f s > dt_required = %.4f s'.format(self.model.dt, dt0))
+
+        # check the numerical dispersion condition: dx <= vmin/(10*f0)
+        dx0 = np.min(self.model.vp) / self.source.f0 / 10.
+        f00 = np.min(self.model.vp) / self.model.dx / 10.
+
+        if dx0 < self.model.dx:
+            print('Warning: modeling dispersion, dx = %.2f m > dx_required =  %.2f m' %(self.model.dx, dx0))
+            print('Warning: modeling dispersion, f0 = %.2f Hz > f0_required = %.2f Hz' %(self.source.f0, f00))
+  
+        # check the source location
+        if (self.source.coord[:,0].min() < self.model.x.min() or 
+            self.source.coord[:,0].max() > self.model.x.max() or
+            self.source.coord[:,1].min() < self.model.z.min() or 
+            self.source.coord[:,1].max() > self.model.z.max()):
+            raise ValueError('Source location out of model range.')
+        
+        # check the receiver location
+        if (self.receiver.coord[:,0].min() < self.model.x.min() or
+            self.receiver.coord[:,0].max() > self.model.x.max() or
+            self.receiver.coord[:,1].min() < self.model.z.min() or
+            self.receiver.coord[:,1].max() > self.model.z.max()):
+            raise ValueError('Receiver location out of model range.')
+
+        # check the mpi number and the source number
+        if self.config.mpi_num > self.source.num:
+            print('Warning: mpi number is larger than source number, reset to source number.')
+            self.config.mpi_num = self.source.num
+
+    def __info__(self):
+        ''' Print solver information
+        '''
+        print('*****************************************************')
+        print('\n    Seismic Waveform Inversion Toolbox: Forward    \n')
+        print('*****************************************************\n')
+        print('Solver: nt = {}, dt = {} ms, time = {} s'.format(self.model.nt, self.model.dt * 1000, self.model.t[-1]))
+        print('Solver: nx = {}, nz = {}, dx = {} m'.format(self.model.nx, self.model.nz, self.model.dx))
+        print('Solver: x  = 0 ~ {} km'.format(self.model.x[-1]/1000))
+        print('Solver: z  = 0 ~ {} km'.format(self.model.z[-1]/1000))        
+        print('Solver: vp = {} ~ {} m/s'.format(self.model.vp.min(), self.model.vp.max()))
+        print('Solver: Source number = {}'.format(self.source.num))
+        print('Solver: Receiver component = {}'.format(self.receiver.comp))
+        print('Solver: {} task in parallel\n'.format(self.config.mpi_num))
